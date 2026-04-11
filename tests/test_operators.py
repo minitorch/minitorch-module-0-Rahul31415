729b8eb63abfcf2f041f5340f9b0af1e04ad9288
Rahul <sning416@gmail.com>
2026-04-11 21:31:58 +0800

    feat(task0.3) add test_sum_distribute

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -184,8 +184,11 @@ def test_sum_distribute(ls1: List[float], ls2: List[float]) -> None:
     """Write a test that ensures that the sum of `ls1` plus the sum of `ls2`
     is the same as the sum of each element of `ls1` plus each element of `ls2`.
     """
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError("Need to implement for Task 0.3")
+    left_side = minitorch.operators.sum(ls1) + minitorch.operators.sum(ls2)
+    sum_of_pairs = [a + b for a, b in zip(ls1, ls2)]
+    right_side = minitorch.operators.sum(sum_of_pairs)
+
+    assert_close(left_side, right_side)
 
 
 @pytest.mark.task0_3
